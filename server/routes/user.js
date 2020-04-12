aa8ec55ac6e8255609f50c5756236d3736782242
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-12 17:51:17 +0200

    added more routes for user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const User = require('../models/user');
+const { User } = require('../models/user');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
+//---------- USER AUTH -------------------
+router.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history,
+  });
+});
+
+// ----------------- REGISTER ROUTE ----------------
 router.post('/api/users/register', async (req, res) => {
   // Create a new user-register new user
   try {
@@ -16,6 +31,7 @@ router.post('/api/users/register', async (req, res) => {
   }
 });
 
+// ------------ LOGIN ROUTE ------------------
 router.post('/api/users/login', async (req, res) => {
   //Login a registered user
   try {
@@ -31,10 +47,38 @@ router.post('/api/users/login', async (req, res) => {
   }
 });
 
-// this route is to get the user profile
+// ---------- GET USER PROFILE ROUTE -------------------
 router.get('/api/users/me', auth, async (req, res) => {
   // View logged in user profile
+
   res.send(req.user);
+});
+
+//--------- LOGOUT ROUTE -------------
+router.post('/api/users/me/logout', auth, async (req, res) => {
+  // Log user out of the application
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token != req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// -------------- LOGOUT ALL ROUTE -----------------
+router.post('/api/users/me/logoutall', auth, async (req, res) => {
+  // Log user out of all devices
+  try {
+    // splice array method to remove tokens from users tokens array. Then save user document.
+    req.user.tokens.splice(0, req.user.tokens.length);
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // generate a token
@@ -46,4 +90,5 @@ router.get('/api/users/me', auth, async (req, res) => {
   });
 });
 });*/
+
 module.exports = router;
