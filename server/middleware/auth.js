cab155e219d1011f6a1602ca5c9177f220dc6a27
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-12 16:21:19 +0200

    fixed code errors

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,11 @@
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
 const auth = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  const data = jwt.verify(token, process.env.JWT_KEY);
+  // get token from request header. Token comes like "Bearer[space] token"- replace Bearer[space] with nothing('')
+  const token = req.header('Authorization').replace('Bearer ', ''); // access token
+  const data = jwt.verify(token, process.env.SECRET);
+  // use jwt.verify() to see if token receive is valid/was created
   try {
     const user = await User.findOne({ _id: data._id, 'tokens.token': token });
     if (!user) {
@@ -16,4 +18,5 @@ const auth = async (req, res, next) => {
     res.status(401).send({ error: 'Not authorized to access this resource' });
   }
 };
+
 module.exports = auth;
