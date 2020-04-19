0f85aa3ff0fe56b699c83ca87ac278f352fe3001
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-19 12:50:20 +0200

    added articles & articles by Id routes

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
 //objectId = require('mongoose').Types.ObjectId();
 // Models
@@ -9,41 +9,62 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
 const router = express.Router();
-//router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: true }));
+
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=100&skip=5
+router.get('/api/product/articles', (req, res) => {
+  // set the order
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id'; // set sortBy
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100; // set limit
+  // Bring from database
+  Product.find()
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.send(articles);
+    });
+});
 
 // ------------- FETCH PRODUCTS/ARTICLES BY ID -----------------
 router.get('/api/product/articles_by_id', (req, res) => {
-  /*let ids = req.query.id.split(',');
-  Product.find({ ids })
-    .then((doc) => {
-      if (!doc) {
-        return res.status(404).end();
-      }
-      return res.status(200).json(doc);
-    })
-    .catch((err) => next(err));*/
+  let id = req.query.id.split(',');
 
-  /*let item = req.query.id;
-  Product.find({ _id: item._id }).then((result) => {
-    assert(result._id.toString() === item._id.toString());
-    done();
-  });*/
-  let type = req.query.type;
+  // Making request
+  Product.find({ _id: { $in: id } })
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });
+
+  //let item = req.query.id.split(',');
+  //res.json(item);
+  /*let type = req.query.type;
   let items = req.query.id;
 
   if (type === 'array') {
     let ids = req.query.id.split(',');
     items = []; // create split for ids // convert to an array
-
     items = ids.map((item) => {
       //console.log(items);
       return mongoose.Types.ObjectId(item); // convert to obj id from mongoose .isValid
     });
   }
   // MAKING THE REQUEST
-  Product.findById({ _id: { $in: items } }).exec((err, docs) => {
-    return res.status(200).send(docs);
-  });
+  Product.find({ _id: { $in: items } })
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs) => {
+      return res.status(200).send(docs);
+    });*/
 });
 
 // ------------- POST A PRODUCT -------------------
