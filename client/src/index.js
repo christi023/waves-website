e3792ffaf55c62eee97e4b8c96f6c3f95d5c3123
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-21 16:10:35 +0200

    added redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './Resources/css/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+// connects with redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
