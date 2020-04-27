c9175c531e6b6d49f114358b9e72b5f2b334bdae
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-27 10:46:21 +0200

    imported action types

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,7 +1,10 @@
 import { LOGIN_USER } from '../actions/types';
+import { REGISTER_USER } from '../actions/types';
 
 export default function (state = {}, action) {
   switch (action.type) {
+    case REGISTER_USER:
+      return { ...state, registerSuccess: action.payload };
     case LOGIN_USER:
       return { ...state, loginSuccess: action.payload };
     default:
