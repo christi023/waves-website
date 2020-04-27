e97bd78bb70efa798d0b6afefa0efd872ba27e5a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-27 10:41:58 +0200

    holds actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,4 +1,28 @@
 import axios from 'axios';
-
+//import from types
+import { REGISTER_USER } from './types';
+import { LOGIN_USER } from './types';
 
 import { USER_ROUTES } from '../components/utils/misc';
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post(`${USER_ROUTES}/register`, dataToSubmit)
+    .then((response) => response.data);
+
+  return {
+    type: REGISTER_USER,
+    payload: request,
+  };
+}
+
+export function loginUser(dataToSubmit) {
+  const request = axios
+    .post(`${USER_ROUTES}/login`, dataToSubmit)
+    .then((response) => response.data);
+
+  return {
+    type: LOGIN_USER,
+    payload: request,
+  };
+}
