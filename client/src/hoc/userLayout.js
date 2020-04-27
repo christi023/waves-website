29eb8545c1f988f466e4b5cf54730052fc3dfb3e
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-27 09:36:01 +0200

    added user layout

---
+++ client/src/hoc/userLayout.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// create variable to host links
+const links = [
+  {
+    name: 'My account',
+    linkTo: '/user/dashboard',
+  },
+  {
+    name: 'User information',
+    linkTo: '/user/user_profile',
+  },
+  {
+    name: 'My cart',
+    linkTo: '/user/cart',
+  },
+];
+
+// render a private route, display user layout
+const UserLayout = (props) => {
+  const generateLinks = (links) =>
+    links.map((item, i) => (
+      <Link to={item.linkTo} key={i}>
+        {item.name}
+      </Link>
+    ));
+
+  return (
+    <div className="container">
+      <div className="user_container">
+        <div className="user_left_nav">
+          <h2>My account</h2>
+          <div className="links">{generateLinks(links)}</div>
+        </div>
+        <div className="user_right">{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserLayout;
