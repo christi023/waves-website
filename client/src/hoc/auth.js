927b419214407ef4ad7e3417e554a675656e946e
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-04-30 19:18:01 +0200

    added auth

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -3,13 +3,33 @@ import { connect } from 'react-redux';
 import { auth } from '../actions/userActions';
 // is user is not Authenticated, show circular progress
 import CircularProgress from '@material-ui/core/CircularProgress';
-
+// Preventing Routing
+import { withRouter } from 'react-router';
 // composed component- function that returns a class/function
 export default function (ComposedClass, reload, adminRoute = null) {
   class AuthCheck extends Component {
     state = {
       loading: true,
     };
+
+    componentDidMount() {
+      this.props.dispatch(auth()).then((response) => {
+        let user = this.props.user.userData;
+        console.log(user);
+
+        if (!user.isAuth) {
+          if (reload) {
+            this.props.history.push('/register_login');
+          }
+        } else {
+          /*if (reload === false) {
+            this.props.history.push('/user/dashboard');
+          }*/
+        }
+
+        this.setState({ loading: false });
+      });
+    }
 
     render() {
       if (this.state.loading) {
@@ -19,7 +39,7 @@ export default function (ComposedClass, reload, adminRoute = null) {
           </div>
         );
       }
-      return <div>component</div>;
+      return <ComposedClass {...this.props} user={this.props.user} />;
     }
   }
   function mapStateToProps(state) {
@@ -27,5 +47,5 @@ export default function (ComposedClass, reload, adminRoute = null) {
       user: state.user,
     };
   }
-  return connect(mapStateToProps)(AuthCheck);
+  return connect(mapStateToProps)(withRouter(AuthCheck));
 }
