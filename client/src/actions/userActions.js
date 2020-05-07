7f19be3a7257a23981be10ac7b8c42e286e942ce
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-05-07 17:37:07 +0200

    added all actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 //import from types
-import { REGISTER_USER, LOGIN_USER, AUTH_USER } from './types';
+import { REGISTER_USER, LOGIN_USER, LOGOUT_USER, AUTH_USER } from './types';
 
-// import from utils
+// import from utils - USER ROUTES
 import { USER_ROUTES } from '../components/utils/misc';
 
 export function registerUser(dataToSubmit) {
@@ -16,6 +16,7 @@ export function registerUser(dataToSubmit) {
   };
 }
 
+// Login User
 export function loginUser(dataToSubmit) {
   const request = axios
     .post(`${USER_ROUTES}/login`, dataToSubmit)
@@ -27,6 +28,17 @@ export function loginUser(dataToSubmit) {
   };
 }
 
+// Logout User
+export function logoutUser() {
+  const request = axios.get(`${USER_ROUTES}/me/logout`).then((response) => response.data);
+
+  return {
+    type: LOGOUT_USER,
+    payload: request,
+  };
+}
+
+// auth
 export function auth() {
   const request = axios.get(`${USER_ROUTES}/auth`).then((response) => response.data);
 
