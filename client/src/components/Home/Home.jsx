1706c2a41b631629b61d0893e5b4a5df17985da6
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-05-10 19:35:21 +0200

    Home slider & Home promotion implemented into Home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+// component
+import HomeSlider from '../Home/HomeSlider';
+import HomePromotion from '../Home/HomePromotion';
 
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <HomeSlider />
+        <HomePromotion />
+      </div>
+    );
   }
 }
