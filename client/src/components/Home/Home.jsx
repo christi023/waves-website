2815c94c4c5dec56a1f4e29ef12070c1ff3b1740
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-05-11 17:14:28 +0200

    Home slider & Home promotion implemented into Home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 // component
 import HomeSlider from '../Home/HomeSlider';
 import HomePromotion from '../Home/HomePromotion';
+// include redux
+import { connect } from 'react-redux';
+// import functions where things are dispatched
+import { getProductsBySell, getProductsByArrival } from '../../actions/productsActions';
+// CardBlock from utils export
+import CardBlock from '../utils/CardBlock';
 
-export default class Home extends Component {
+class Home extends Component {
+  componentDidMount() {
+    this.props.dispatch(getProductsBySell());
+    this.props.dispatch(getProductsByArrival());
+  }
   render() {
     return (
       <div>
         <HomeSlider />
+        <CardBlock list={this.props.products.bySell} title="Best Selling Guitars" />
         <HomePromotion />
+        <CardBlock list={this.props.products.byArrival} title="New Arrivals" />
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.products,
+  };
+};
+export default connect(mapStateToProps)(Home);
