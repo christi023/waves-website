927e9c5868bbfd69474873c28d043ead26954198
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-05-13 14:06:24 +0200

    Added shop route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// components
+// components - public
 import Home from './components/Home/Home';
 import Register_Login from './components/Register_Login/RegisterLogin';
 import Register from './components/Register/Register';
+import Shop from './components/Shop/Shop';
+// admin
 import UserDashboard from './components/UserDashboard/UserDashboard';
+
 // HOC imports
 import Layout from './hoc/layout';
 import Auth from './hoc/auth';
@@ -15,10 +18,12 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact component={Auth(Home, null)} />
         <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)} />
+
         <Route path="/register" exact component={Auth(Register, false)} />
         <Route path="/register_login" exact component={Auth(Register_Login, false)} />
+        <Route path="/" exact component={Auth(Home, null)} />
+        <Route path="/shop" exact component={Auth(Shop, null)} />
       </Switch>
     </Layout>
   );
